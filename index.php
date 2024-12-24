7b0bbe4ebfff314eb8a5e1015e5a439d2596b175
Advis Com <staycalm975@gmail.com>
2024-12-24 21:01:47 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione error similique expedita cum vero perspiciatis a, asperiores quam corporis nihil alias quod magnam soluta. Deserunt vero maiores animi accusamus rerum!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
